5bda7b703eb0706ec3731ca7840d2f742a7bd836
Dale LaPlante <bmbmjmdm@gmail.com>
2024-12-15 16:54:04 +0100

    finish allergen list

--- src/Allergens.ts
+++ src/Allergens.ts
@@ -4,7 +4,12 @@ export const listOfAllergens:string[] = [
   "Elastane",
   "Nylon",
   "Spandex",
-  "Rayon",
+  "Lycra",
+  // Synonyms
+  "chloroprene",
+  "elastic",
+
+
 
 
   // Carba Mix (MX-06)
@@ -12,12 +17,10 @@ export const listOfAllergens:string[] = [
   "MX-06",
   // Synonyms
   "Carbamates",
-  // Composition
   "Diphenylguanidine",
   "DPG",
   "Noccel D",
   "Sanceler D",
-  "Soxinol D",
   "Zinc Dibutyldithiocarbamate",
   "Butasan",
   "Butazate",
@@ -33,8 +36,6 @@ export const listOfAllergens:string[] = [
   "Ethazate",
   "Ethyl Zimate",
   "Nocceler EZ",
-  "Soxinol EZ",
-  // Online synonyms
   "zinc bis(diethyldithiocarbamate)",
   "bis(N,N-dibutyldithiocarbamato)zinc",
   "carbamic acid dibutyldithio",
@@ -64,6 +65,7 @@ export const listOfAllergens:string[] = [
   "D-049E",
   // Synonyms
   "2-Bromo-2",
+  "dibromoglutaronitrile",
   "bromomethyl glutaronitrile",
   "bromomethyl pentanedinitrile",
   "Dibromo dicyanobutane",
@@ -72,7 +74,6 @@ export const listOfAllergens:string[] = [
   "Merquat 2200",
   "Phenoxyethanol",
   "Tektamer 38",
-  // Online synonyms
   "Bromothalonil",
   "Euxyl K400",
   "Metacide 38",
@@ -84,16 +85,134 @@ export const listOfAllergens:string[] = [
   "1,2-Dibrom-2,4-dicyanobutan",
   "Dibromdicyanobutan",
   "Fenoksyetanol",
-  "Bromotalonil"
+  "Bromotalonil",
+  "Dibromoglutaronitril",
   
 
-  
+  // Toluenesulfonamide formaldehyde resin (T-010)
+  "Toluenesulfonamide",
+  "T-010",
+  "formaldehyde resin",
+  // Synonyms
+  "Benzene sulfonamide",
+  "4-methyl-polymer",
+  "Mearlmaid TX-EPM-8-6",
+  "Santolite MHP",
+  "Santolite MS",
+  "Tosylamide/formaldehyde",
+  "Tosylamide resin",
+  // Norsk
+  "Toluenesulfonamid",
+  "Formaldehydharpiks",
+  "Benzenesulfonamid",
+  "4-metylpolymer",
+  "Tosylamid/formaldehyd",
+  "Tosylamidharpiks",
+
+  // 2-Bromo-2-nitropropane-1,3-diol (B-015B)
+  "nitropropane",
+  "3-diol",
+  "B-015B",
+  // Synyonyms
+  "2-Nitro-2",
+  "propanediol",
+  "Bronidiol",
+  "Bronocot",
+  "Bronopol",
+  "Bronosol",
+  "Bronotak",
+  "HSDB 7195",
+  "Lexgard bronopol",
+  "NSC 141021",
+  "Onyxide 500",
+  "nitrotrimethyleneglycol",
+  "b-Bromo-b",
+  "myacide",
+  "bromo-2-nitro",
+  "bromo-1-nitro",
+  "nitro-2-bromo",
+  "nitro-1-bromo",
+  // Norsk
+  "Nitropropan",
+  "Propandiol",
+  "Nitrotrimetylenglykol",
+
+  // Colophony (C-020)
+  "Colophony",
+  "C-020",
+  // Synonyms
+  "Disproportionated rosin",
+  "Disproportionated resin",
+  "Gum resin",
+  "Wood resin",
+  "Pine resin",
+  "Pine gum",
+  "Wood Gum",
+  "Colophony",
+  "Abietic alcohol",
+  "Abietyl alcohol",
+  "Colophonium",
+  "Rosin",
+  "Tall oil",
+  "Abietic acid",
+  "Abietic Alcohol",
+  "Methyl Abietate",
+  "Abitol",
+  // Norsk
+  "Kolofonium",
+  "Disproporsjonert harpiks",
+  "Gummiharpiks",
+  "Treharpiks",
+  "Furuharpiks",
+  "Abietisk alkohol",
+  "Abietylalkohol",
+  "Metylabietatalkohol",
+  "Tretjære",
+  "Tallolje",
+  "Abietinsyre",
+  "Abietisk alkohol",
+  "Metylabietat",
+  "Furu harpiks",
+  "Treharpiks",
+
+  // 2-Hydroxyethylmethacrylate (H-010)
+  "Hydroxyethylmethacrylate",
+  "H-010",
+  // Synonyms
+  "glycol methacrylate",
+  "glycol monomethacrylate",
+  "Mhoromer",
+  "heme-a",
+  "Propenoic acid",
+  "2-methyl,2-hydroxyethyl",
+  "GMA",
+  "Ethylene glycol methacrylate",
+  "HEMA",
+  "(methacryloyloxy)ethanol",
+  // Norsk
+  "Hydroksyetylmetakrylat",
+  "Glykolmetakrylat",
+  "Glykolmonometakrylat",
+  "Propenosyre",
+  "2-Metyl,2-hydroksyetyl",
+  "Etylenglykolmetakrylat",
+  "(Metakryloyloxy)etanol",
+
+  // Cobalt (C-017A)
+  "Cobalt",
+  "(C-017A)",
+  // Synonyms
+  "hexahydrate",
+  "CCRIS 7729",
+  // Norsk
+  "Kobolt",
+  "Hexahydrat",
   
 ]
 
 export const containsAllergen = (aiDescription: string, allergen: string):boolean => {
   /* TODO determine if its in the string. this is difficult because the strings may be different for a variet of reasons: hyphens, paranthesis, spaces, capitalization, roman numerals vs numbers, even language (norsk vs engelsk) or chemical categorization (US D-# T-# MX-# system vs Norway's E# system etc) */
-  // I could get around some of this by stripping capitalization, paranthesis, hyphens, spaces, etc
+  // I could get around some of this by stripping capitalization, paranthesis, hyphens, spaces, slashes, etc
   // I MIGHT be able to get around the english-norsk barrier by asking the ai to convert my listOfAllergens into other languages as well and add those to the listOfAllergens source code
   // see TODO in App.tsx
 }
