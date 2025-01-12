5f63bb87cd385383a775614b5ef9765aafc7d88c
Dale LaPlante <bmbmjmdm@gmail.com>
2025-01-12 13:48:33 +0100

    add double checking, text input, translation, and reverse search

--- src/AI.ts
+++ src/AI.ts
@@ -1,8 +1,13 @@
 import Anthropic from '@anthropic-ai/sdk';
+import { allergensWithoutCodes, listOfAllergens, minimalistAllergenList } from './Allergens';
 
 const client = new Anthropic({
   apiKey: "",
 });
+
+const model =  "claude-3-5-sonnet-20241022"
+// claude-3-haiku-20240307 for faster and cheaper
+// claude-3-5-sonnet-20241022 for better results
 
 export async function readImage(mediaType: "image/jpeg" | "image/png", mediaData: string) {
   const message = await client.messages.create({
@@ -26,9 +31,58 @@ export async function readImage(mediaType: "image/jpeg" | "image/png", mediaData
           ],
       }
     ],
-    model: "claude-3-5-sonnet-20241022",
-    // claude-3-haiku-20240307 for faster and cheaper
-    // claude-3-5-sonnet-20241022 for better results
+    model: model
+  });
+  return message
+}
+
+export async function translateText(text: string) {
+  const message = await client.messages.create({
+    max_tokens: 2048,
+    messages: [
+      {
+          "role": "user",
+          "content": [
+              {
+                  "type": "text",
+                  "text": "This is the label on a bottle. Your job is to translate the text into English."
+              },
+              {
+                  "type": "text",
+                  "text": text
+              }
+          ],
+      }
+    ],
+    model: model
+  });
+  return message
+}
+
+
+export async function doubleCheckList(text: string) {
+  const message = await client.messages.create({
+    max_tokens: 2048,
+    messages: [
+      {
+          "role": "user",
+          "content": [
+              {
+                  "type": "text",
+                  "text": "I will provide 2 lists of text. The first list is the ingredient label on a bottle. The second list are known allergens. Your job is to see if any ingredients on the bottle matches any of the known allergens. You should consider all known synonyms as well as compounds that might be similar enough to also cause a reaction. Do not consider what other individuals are sensitive to, only the list."
+              },
+              {
+                  "type": "text",
+                  "text": "=======BOTTLE LABEL=======\n" + text
+              },
+              {
+                  "type": "text",
+                  "text": "=======LIST OF CHEMICAL/SUBSTANCE/INGREDIENT NAMES=======\n" + minimalistAllergenList.join("\n")
+              }
+          ],
+      }
+    ],
+    model: model
   });
   return message
 }
--- src/App.tsx
+++ src/App.tsx
@@ -14,19 +14,25 @@ import {
   StyleSheet,
   Text,
   View,
+  TextInput,
 } from 'react-native';
 import {ImagePickerResponse, ImageLibraryOptions, launchCamera, launchImageLibrary} from 'react-native-image-picker';
 import { TextBlock } from '@anthropic-ai/sdk/resources/messages.mjs';
 import {Section, SectionText} from "./Section"
-import { readImage } from "./AI"
+import { readImage, translateText, doubleCheckList } from "./AI"
 import {listOfAllergens, containsAllergen} from "./Allergens"
-
+import CheckBox from 'react-native-check-box'
+import { Message } from '@anthropic-ai/sdk/src/resources/messages.js';
 
 function App(): React.JSX.Element {
   const [results, setResults] = useState("")
   const [details, setDetails] = useState("")
   const [showList, setShowList] = useState(false)
   const [meta, setMeta] = useState("")
+  const [textInputText, setTextInputText] = useState("")
+  const [translate, setTranslate] = useState(true)
+  const [askAI, setAskAI] = useState(true)
+  const [reverseSearch, setReverseSearch] = useState(false)
 
   const onCamera = () => {
     onPhoto(launchCamera)
@@ -64,36 +70,94 @@ function App(): React.JSX.Element {
             setDetails("Not png or jpeg")
           }
           else {
+            setResults("AI Vision...")
+            setDetails("AI Vision...")
             const data = result.assets[0].base64
             const type = result.assets[0].type
             const aiResponse = await readImage(type, data)
-            if (!aiResponse.content || !aiResponse.content.length) {
-              setResults("Errernous response from ai")
-              setDetails("No content")
-            }
-            else {
-              const aiDescription = (aiResponse.content[0] as TextBlock).text
-              if (!aiDescription) {
-                setResults("Errernous response from ai")
-                setDetails("Response type isn't text or text is empty")
-              }
-              else {
-                const listOfDanger = []
-                for (const allergen of listOfAllergens) {
-                  if (containsAllergen(aiDescription, allergen)) {
-                    listOfDanger.push(allergen)
-                  }
-                }
-                // TODO we could now pass the aiDescription and listOfAllergens to the AI now and ask it if there's any overlap, to get a second set of eyes
-                listOfDanger.unshift(listOfDanger.length ? "DANGER" : "Safe")
-                setResults(listOfDanger.join('\n'))
-                setDetails(aiDescription)
-                setMeta(JSON.stringify({stop_reason: aiResponse.stop_reason, usage: aiResponse.usage}))
-              }
+            const aiDescription = vetAIResult(aiResponse, "Vision")
+            if (aiDescription) {
+                processTextForAllergens(aiDescription)
             }
           }
         }
       }
+    }
+    catch (e) {
+      setResults("Thrown error")
+      if (typeof e === "string") {
+        setDetails(e)
+      }
+      else if (e instanceof Error) {
+        setDetails(e.message)
+      }
+      else {
+        setDetails(JSON.stringify(e))
+      }
+    }
+  }
+
+  const vetAIResult = (aiResponse: Message, aiQuestion: string):string|null => {
+    if (!aiResponse.content || !aiResponse.content.length) {
+      setResults("Errernous response from ai during " + aiQuestion)
+      setDetails("No content")
+      return null
+    }
+    else {
+      const aiDescription = (aiResponse.content[0] as TextBlock).text
+      if (!aiDescription) {
+        setResults("Errernous response from ai during " + aiQuestion)
+        setDetails("Response type isn't text or text is empty")
+        return null
+      }
+      else {
+        setMeta((meta) => meta + "\n" + aiQuestion + ": " + JSON.stringify({stop_reason: aiResponse.stop_reason, usage: aiResponse.usage}))
+        return aiDescription
+      }
+    }
+  }
+
+  const processTextForAllergens = async (text:string) => {
+    try {
+      let translation = ""
+      if (translate) {
+        setResults("Translating...")
+        setDetails("Translating...")
+        const translationResponse = await translateText(text)
+        const translationText = vetAIResult(translationResponse, "Translation")
+        if (translationText) translation = "\n\n" + translationText
+      }
+      // now go through all of our allergens, the text, and the translated text, and see if we have any matches
+      const listOfDanger = []
+      for (const allergen of listOfAllergens) {
+        let contains = false
+        // reverse search searches each allergen for the text input
+        // normal search searches the text for each allergen
+        if (reverseSearch) contains = containsAllergen(allergen, text)
+        else contains = containsAllergen(text, allergen)
+        // if we got a translation from the ai, search that as well
+        if (translation) {
+          if (reverseSearch) contains = contains || containsAllergen(allergen, translation)
+          else contains = contains || containsAllergen(translation, allergen)
+        }
+        if (contains) {
+          listOfDanger.push(allergen)
+        }
+      }
+      // now ask the AI for a second set of eyes to see if we missed any synonyms, etc
+      let doubleChecked = ""
+      if (askAI) {
+        setResults("AI double checking...")
+        setDetails("AI double checking...")
+        const doubleCheckResponse = await doubleCheckList(text)
+        const doubleCheckText = vetAIResult(doubleCheckResponse, "Double Check")
+        if (doubleCheckText) {
+          doubleChecked = "\n\n" + doubleCheckText
+        }
+      }
+      listOfDanger.unshift(listOfDanger.length ? "DANGER" : "Safe")
+      setResults(listOfDanger.join('\n') + doubleChecked)
+      setDetails(text + translation)
     }
     catch (e) {
       setResults("Thrown error")
@@ -122,12 +186,25 @@ function App(): React.JSX.Element {
         <View
           style={{ backgroundColor: "#000000"}}>
           <Section title="Capture">
+              <CheckBox rightTextStyle={{color: "#ffffff"}} checkBoxColor='#ffffff' style={styles.checkbox} isChecked={translate} onClick={() => setTranslate(!translate)} rightText={"Translate"} />
+              <CheckBox rightTextStyle={{color: "#ffffff"}} checkBoxColor='#ffffff' style={styles.checkbox} isChecked={askAI} onClick={() => setAskAI(!askAI)} rightText={"AI Double-Check"} />
+              <CheckBox rightTextStyle={{color: "#ffffff"}} checkBoxColor='#ffffff' style={styles.checkbox} isChecked={reverseSearch} onClick={() => {
+                setAskAI(reverseSearch)
+                setTranslate(reverseSearch)
+                setReverseSearch(!reverseSearch)
+              }} rightText={"Reverse Search (used to lookup allergens in your list)"} />
             <TouchableOpacity onPress={onCamera} style={styles.button}>
               <Text style={styles.buttonText}>Camera</Text>
             </TouchableOpacity>
             <TouchableOpacity onPress={onFile} style={styles.button}>
-            <Text style={styles.buttonText}>File</Text>
+              <Text style={styles.buttonText}>File</Text>
             </TouchableOpacity>
+            <View style={{height: 150, width: "100%", flexDirection: "row", alignItems: "center", justifyContent: "center"}}>
+              <TextInput multiline style={styles.textInput} onChangeText={setTextInputText} />
+              <TouchableOpacity onPress={() => {setMeta(""); processTextForAllergens(textInputText)}} style={styles.button}>
+                <Text style={styles.buttonText}>Text</Text>
+              </TouchableOpacity>
+            </View>
           </Section>
           <Section title="Results">
             <SectionText>{results}</SectionText>
@@ -167,6 +244,17 @@ const styles = StyleSheet.create({
   buttonText: {
     color: "#FFFFFF",
     fontSize: 20
+  },
+  checkbox: {
+    padding: 6,
+  },
+  textInput: {
+    flex: 1,
+    color: "#FFFFFF",
+    backgroundColor: "#444444",
+    padding: 12,
+    margin: 12,
+    minHeight: 120
   }
 });
 
